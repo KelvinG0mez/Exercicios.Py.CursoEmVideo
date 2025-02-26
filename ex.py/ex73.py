b6dbb33c5fd5c0c651bff55cbe188b8af059b351
Kelvin Gomes D'Beraldini <kelvingomes888@gmail.com>
2025-02-26 18:20:45 -0300

    Create ex73.py

---
+++ ex.py/ex73.py
@@ -0,0 +1,16 @@
+times = (
+    'Botafogo', 'Palmeiras', 'Flamengo', 'Fortaleza',
+    'Internacional', 'São Paulo', 'Corinthians', 'Bahia',
+    'Cruzeiro', 'Vasco da Gama', 'EC Vitória', 'Atlético-MG',
+    'Fluminense', 'Grêmio', 'Juventude', 'Bragantino',
+    'Athletico-PR', 'Criciúma', 'Atlético-GO', 'Cuiabá'
+)
+print('=-'*15)
+print(f'Lista de times do Brasileirão: {times}')
+print('=-'*15)
+print(f'Os 5 primeiros são: {times[:5]}')
+print('=-'*15)
+print(f'Os 4 ultimos são: {times[-4:]}')
+print('=-'*15)
+print(f'Times em ordem alfabetica: {sorted(times)}')
+print('=-'*15)
